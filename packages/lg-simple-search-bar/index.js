9e84ef8f38b39ca01650bc31d8e84d6f9e627366
zztdan <zztdandanwork@gmail.com>
2019-01-10 11:06:55 +0800

    修改版本号到10

--- packages/lg-simple-search-bar/index.js
+++
@@ -1,8 +0,0 @@
-import LgSimpleSearchBar from './src/main';
-
-/* istanbul ignore next */
-LgSimpleSearchBar.install = function(Vue) {
-  Vue.component(LgSimpleSearchBar.name, LgSimpleSearchBar);
-};
-
-export default LgSimpleSearchBar;
